23959d510974490012958481a1c112d6fd32abf0
Tubo Shi <shitubo@gmail.com>
2018-10-24 23:41:59 +1300

    Image now works, using async/await methods

--- src/DocumentGeneration.js
+++ src/DocumentGeneration.js
@@ -3,50 +3,60 @@ import saveAs from 'file-saver'
 import JSZip from 'jszip'
 import JSZipUtils from 'jszip-utils'
 import Docxtemplater from 'docxtemplater'
+import ImageModule from 'open-docxtemplater-image-module'
 
 import template from "./report_template.docx"
 
 
-export default function generateDoc(data) {
-    generate_document(data);
+export default async function loadImages(data) {
+    const image = await new Response(data.lesions[0].images.t2w).arrayBuffer();
+
+    generate_document(data, image)
 }
 
-function loadFile(url,callback){
-    JSZipUtils.getBinaryContent(url,callback);
+function loadFile(url, callback) {
+    JSZipUtils.getBinaryContent(url, callback);
 }
 
-function generate_document() {
-    loadFile(template, function(error,content){
-        if (error) { throw error }
+function generate_document(data, image) {
+    loadFile(template, function (error, content) {
+        if (error) {
+            throw error
+        }
 
         const zip = new JSZip(content),
-            doc=new Docxtemplater().loadZip(zip);
+            imageModule = new ImageModule({
+                centered: false,
+                getImage: (file) => file,
+                getSize: () => [200, 200],
+            }),
+            doc = new Docxtemplater()
+                .attachModule(imageModule)
+                .loadZip(zip);
 
         doc.setData({
             name: 'John',
-            last_name: 'Doe',
-            phone: '0652455478',
-            description: 'New Website'
+            image: image,
         });
+
         try {
-            // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
             doc.render()
         }
         catch (error) {
-            var e = {
+            const e = {
                 message: error.message,
                 name: error.name,
                 stack: error.stack,
                 properties: error.properties,
-            }
+            };
             console.log(JSON.stringify({error: e}));
-            // The error thrown here contains additional information when logged with JSON.stringify (it contains a property object).
             throw error;
         }
-        var out=doc.getZip().generate({
-            type:"blob",
+        const out = doc.getZip().generate({
+            type: "blob",
             mimeType: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-        })
-        saveAs(out,"report.docx")
+        });
+        saveAs(out, "report.docx")
     })
 }
+
